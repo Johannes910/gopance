5be61c2ba6ad5dcef086e15606c979581d05d4b8
Johannes Herzog-Ahorn <48024528+Johannes910@users.noreply.github.com>
2022-09-01 16:57:25 -0500

    Corregido header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import defaultpfp from "../assets/images/defaultpfp.png"
 
 export const Header = () => {
 
-  return(
+	return(
 
     <>
       <header className="p-3 text-bg-dark">
@@ -11,37 +11,33 @@ export const Header = () => {
             <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
             </a>
 
-
+            <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+              <li><a href="#" className="nav-link px-2 text-secondary">INICIO</a></li>
+              <li><a href="#" className="nav-link px-2 text-white">ALIMENTOS</a></li>
+              <li><a href="#" className="nav-link px-2 text-white">SERVICIOS</a></li>
+              <li><a href="#" className="nav-link px-2 text-white">TUS PEDIDOS</a></li>
+            </ul>
 
             <div className="col-lg-3">
               <div className="input-group">
                 <input type="text" className="form-control" placeholder="¡Explora todos los productos!" />
                 <span className="input-group-btn">
                   <button className="btn btn-outline-light me-2" type="button">BUSCAR</button>
-  <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-              <li><a href="#" class="nav-link px-5 text-secondary">INICIO</a></li>
-              <li><a href="#" class="nav-link px-5 text-white">ALIMENTOS</a></li>
-              <li><a href="#" class="nav-link px-5 text-white">SERVICIOS</a></li>
-              <li><a href="#" class="nav-link px-5 text-white">TUS PEDIDOS</a></li>
-            </ul>
 
-            <div class="col-lg-4">
-              <div class="input-group">
-                <input type="text" class="form-control" placeholder="¡Explora todos los productos!" />
-                <span class="input-group-btn">
-                  <button class="btn btn-outline-light me-5" type="button">BUSCAR</button>
                 </span>
               </div>
             </div>
 
 
+            <div className="text-end">
+              
             <div>
               <img src={defaultpfp} position="relative" width="50px" height="50px" alt="pfp" />
-
             </div>
 
           </div>
         </div>
+         </div>
       </header>
 
     </>
