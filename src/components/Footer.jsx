8c02565d2c33c25075dc265836c7619add4d5015
Johannes Herzog-Ahorn <48024528+Johannes910@users.noreply.github.com>
2022-09-01 01:04:17 -0500

    footer and header

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+export const Footer = () =>{
+
+
+	return(
+		<>
+			<div class="container">
+  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <p>Feliz compra, Tatiana!</p>
+    <ul class="nav col-md-4 justify-content-end">
+      <li class="nav-item"><p>TU BALANCE</p><p>200.000.000 COP</p></li>
+    </ul>
+  </footer>
+</div>
+			</>
+
+		)
+}
