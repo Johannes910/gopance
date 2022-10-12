08b9a6f74dd4296ca4041e2ae447f2ad6d09e14f
xlgabriel <gabriel.jeannot@uao.edu.co>
2022-10-12 12:40:35 -0500

    Cambio de texto en Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,19 +8,19 @@ export const Footer = () => {
   return (
 
     <>
-  <footer className="footerdiv">
-    <div className="parentFoo">
-<div className="div1Foo"><img src={carrito} className="imgCarrito"></img><b className="bTatiana">Feliz compra, Tatiana</b></div>
-<div className="div2Foo"><img src={logoImg} className="imgLogo"></img> </div>
-<div className="div3Foo"><p className="pbalance">Tu balance <br/><b className= "bdinero">$253.000</b></p> </div>
+      <footer className="footerdiv">
+        <div className="parentFoo">
+          <div className="div1Foo"><img src={carrito} className="imgCarrito"></img><b className="bTatiana">Feliz compra, Perafán</b></div>
+          <div className="div2Foo"><img src={logoImg} className="imgLogo"></img> </div>
+          <div className="div3Foo"><p className="pbalance">Tu balance <br /><b className="bdinero">$253.000</b></p> </div>
 
-</div>
-
-
-  </footer>
+        </div>
 
 
-</>
+      </footer>
+
+
+    </>
 
   )
 }
